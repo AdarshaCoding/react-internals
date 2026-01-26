fa6f51d6d8e411bb440b53e5166405148303c435
AdarshaCoding <adarshapc4467@gmail.com>
2026-01-26 09:42:22 +0530

    React.createElement (a Node) & Vitaul DOM(nested Nodes): plain JS Object with nested elements

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-const heading = React.createElement(
-  "h1",
-  { key: 123, className: "heading1" },
-  "Just a React.createElement",
+/**
+ * <div id="parent">
+ *     <h1 id="child1"> Child 1 </h1>
+ * </div>
+ */
+
+// React.createElement will create a Node and Vitual DOM is a combination of such nodes, just like above exmaple
+
+const parent = React.createElement(
+  "div",
+  { key: 123, id: "parent" },
+  React.createElement("h1", { id: "child1" }, "Child 1"),
 );
 
-console.log(
-  "It's called as React Element and a plain JS Object! but not a Virtual DOM",
-);
-console.log(heading);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
