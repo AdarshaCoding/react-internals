7298fb06d53562daee37af93cc0d04bb24ab2ee3
AdarshaCoding <adarshapc4467@gmail.com>
2026-01-26 09:36:31 +0530

    React.createElement: plain JS Object

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 const heading = React.createElement(
   "h1",
   { key: 123, className: "heading1" },
-  "Hello.......",
+  "Just a React.createElement",
 );
 
+console.log(
+  "It's called as React Element and a plain JS Object! but not a Virtual DOM",
+);
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
